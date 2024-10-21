09d87323b60d00f1cec0181bb7f60cb20d6427d0
lovelysehotra <lsehotra12@gmail.com>
2024-10-21 17:16:26 +0530

    add controller,router,service

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -6,5 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../../.env") })
 export const {
-    PORT
+    PORT,
+    DATABASE_URL,
+    JWT_SECRET
 } = process.env
--- src/config/server.ts
+++ src/config/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
-
+import { connectionToDB } from "@/infrastructure";
+import { appRouter } from "@/interface/routers";
 
 export type AppConfig={
     port?:number |string;
@@ -11,9 +12,12 @@ export class Server{
     constructor (config:AppConfig){
         this.config = config
         this.app = express();
+        this.app.use(express.json());
+        this.app.use("/api",appRouter);
     }
     start(){
         const port = this.config.port ?? 1209;
+        connectionToDB()
         this.app.listen(port,()=>{
             console.log("Yes i am working")
         })
